4b1da7e1f6d5f423124f05ff1b8477b59a869d98
Robert Vollmert <rfvollmert@gmail.com>
2015-05-17 15:09:40 +0200

    Matcher interface for radix tree.

--- radix.go
+++ radix.go
@@ -4,19 +4,36 @@ import (
 	radix "github.com/armon/go-radix"
 )
 
-func AnyContainsAnyRadix(ss []string, patterns []string) bool {
-	r := radix.New()
+type Radix struct {
+	*radix.Tree
+}
+
+var _ Matcher = Radix{}
+
+func MakeRadix(patterns []string) Radix {
+	r := Radix{radix.New()}
 	for _, p := range patterns {
 		r.Insert(p, struct{}{})
 	}
+	return r
+}
 
+func (r Radix) Matches(s string) bool {
+	// i == len(s) handles ""
+	for i := 0; i <= len(s); i++ {
+		_, val, _ := r.LongestPrefix(s[i:])
+		if val != nil {
+			return true
+		}
+	}
+	return false
+}
+
+func AnyContainsAnyRadix(ss []string, patterns []string) bool {
+	r := MakeRadix(patterns)
 	for _, s := range ss {
-		// i == len(s) handles ""
-		for i := 0; i <= len(s); i++ {
-			_, val, _ := r.LongestPrefix(s[i:])
-			if val != nil {
-				return true
-			}
+		if r.Matches(s) {
+			return true
 		}
 	}
 	return false
